3dc05a200ba783dc19626e2edc651f621713c956
kratika-98 <sweety.jharbade999@gmail.com>
2024-03-28 23:07:20 +0530

    back

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
-
+app.use(cors());
 
 mongoose.connect('mongodb+srv://sweetyjharbade999:JEe25hmQ2MvHo26C@kratikadb.darqdux.mongodb.net/empDatas?retryWrites=true&w=majority&appName=kratikadb', {
   useNewUrlParser: true,
